b9b020c38a09a3b2aa1ba53a2dc1297bd7ac2be0
VUVANQUANIT <quancn27@gmail.com>
2025-05-27 15:04:23 +0700

    đã đăng nhập đăng kí thành công

--- src/main/java/com/banhoi/banhoi_api/model/User.java
+++ src/main/java/com/banhoi/banhoi_api/model/User.java
@@ -2,11 +2,13 @@ package com.banhoi.banhoi_api.model;
 
 import jakarta.persistence.*;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
